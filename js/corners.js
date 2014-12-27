6ebdd1916a7fe42635181d302793bded71e2af32
unknown <Alex.Greenstein@ALEX-LP.gizmox.com>
2014-12-27 11:55:20 +0200

    initial commit

---
+++ js/corners.js
@@ -0,0 +1,14 @@
+var Corners = Corners || {
+    // define board that exposes state object (read-only), functions : init(), setChecker(point, checker)
+    board : (function board(){
+        var state =
+            init = function init(){
+        },
+
+
+        return {
+
+        };
+    })(),
+    }
+};
